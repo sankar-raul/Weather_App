cc5713bfa15f336f55f091b26c871b880a6507d9
sankar-raul <raulsankar99@gmail.com>
2024-05-12 19:33:19 +0530

    Section1 Skeleton fixed

--- src/components/skeleton.jsx
+++ src/components/skeleton.jsx
@@ -7,7 +7,7 @@ export default function Section1Skeleton({className}) {
     const Load = components[className]
     return (
         <>
-         <div className={`${className} skeleton`}>
+         <div className={`${className}`}>
             <Load />
          </div>
         </>
@@ -26,7 +26,7 @@ function Section1() {
             <div className="weather-main" style={{gap: "10px"}}>
                 <div className="weather-icon" style={{position: "relative"}}>
                 <Skeleton style={{position: "absolute"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"100%"} height={"100%"}/>
-                    <img src="/src/components/componentAssets/weather/sun-cloud.png" className="icon- skeleton temp-icon" alt="&nbsp; ok"/>
+                    <img src="/src/components/componentAssets/weather/sun-cloud.png" className="icon- temp-icon" alt="&nbsp; ok"/>
                 </div>
                 <div className="temperature">
                     <p className="temp">
@@ -61,24 +61,25 @@ function Section3() {
 }
 function Weatherbox() {
     return (
-        <div className="weatherbox skeleton">
+        <div className="weatherbox">
         <div className="infos" style={{position: "relative"}}>
-        <Skeleton style={{position: "absolute"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"50px"} height={"55%"} inline={true}/>
-            <img src='/src/components/componentAssets/weather/pressure.png' alt="air pressure" className="icon temp-icon" />
-            <p className="value"></p>
-            <p className="name">&nbsp;</p>
+        <Skeleton style={{position: ""}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"50px"} height={"165%"} inline={false}/>
+            <p className="value">&nbsp;</p>
+            <p className="name">
+            <Skeleton style={{position: "relative", marginTop: "0%"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"60px"} height={"45%"} inline={false}/>
+            </p>
         </div>
         <div className="infos" style={{position: "relative"}}>
-        <Skeleton style={{position: "absolute"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"50px"} height={"55%"} inline={true}/>
-        <img src='/src/components/componentAssets/weather/wind.png' alt="wind speed" className="icon temp-icon" />
+        <Skeleton style={{position: ""}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"50px"} height={"165%"} inline={false}/>
             <p className="value">&nbsp;</p>
-            <p className="name">&nbsp;</p>
+            <p className="name">
+                <Skeleton style={{position: "relative", marginTop: "0%"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"60px"} height={"45%"} inline={false}/>
+            </p>
         </div>
         <div className="infos" style={{position: "relative"}}>
-        <Skeleton style={{position: "absolute"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"50px"} height={"55%"} inline={true}/>
-        <img src='/src/components/componentAssets/weather/humidity.png' alt="humidity" className="icon temp-icon" />
+        <Skeleton style={{position: ""}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"50px"} height={"165%"} inline={false}/>
             <p className="value">&nbsp;</p>
-            <p className="name">&nbsp;</p>
+            <p className="name"><Skeleton style={{position: "relative", marginTop: "0%"}} baseColor="#e3F0fdaa" highlightColor="#07affe11" duration={1.3} width={"60px"} height={"45%"} inline={false}/></p>
         </div>
     </div>
     )
